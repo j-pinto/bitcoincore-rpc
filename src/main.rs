5e78a2fdb7dbef5514b1bf66aa09ce0d9e32705a
j-pinto <jfpinto1989@gmail.com>
2025-03-06 21:35:12 -0500

    get new address

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use bitcoincore_rpc::{bitcoin::Address, Auth, Client, RpcApi};
+use bitcoincore_rpc::{ json, Auth, Client, RpcApi};
 fn main() {
     //connect to node
     let rpc_auth = Auth::UserPass("user".to_string(), "password".to_string());
@@ -15,4 +15,12 @@ fn main() {
     let btc_balance = balance.as_btc();
     println!("sats: {:?}", sat_balance);
     println!("btc: {:?}", btc_balance);
+
+    //new address
+    let my_address = rpc
+        .get_new_address(None, Option::Some(json::AddressType::Bech32))
+        .unwrap();
+    println!("my address: {:?}", my_address);
+
+
 }
