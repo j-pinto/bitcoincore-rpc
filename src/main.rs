7f96dd5405486c3035dfc7421d137e1860c96aaa
j-pinto <jfpinto1989@gmail.com>
2025-03-06 21:02:58 -0500

    test with get_mining_info

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use bitcoincore_rpc::{Auth, Client, RpcApi};
 fn main() {
-    println!("Hello, world!");
+    let rpc_auth = Auth::UserPass("user".to_string(), "password".to_string());
+    let rpc_url = "http://192.168.1.2:18332".to_string();
+    
+    let rpc = Client::new(rpc_url, rpc_auth).unwrap();
+
+    let mining_info = rpc.get_mining_info().unwrap();
+    println!("{:?}", mining_info);
 }
