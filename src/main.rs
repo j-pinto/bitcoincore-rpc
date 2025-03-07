dff0b3c5786f3a9447a15ad8ca6d4a8ce4d89f72
j-pinto <jfpinto1989@gmail.com>
2025-03-06 21:23:29 -0500

    get balance

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
-use bitcoincore_rpc::{Auth, Client, RpcApi};
+use bitcoincore_rpc::{bitcoin::Address, Auth, Client, RpcApi};
 fn main() {
+    //connect to node
     let rpc_auth = Auth::UserPass("user".to_string(), "password".to_string());
     let rpc_url = "http://192.168.1.2:18332".to_string();
-    
     let rpc = Client::new(rpc_url, rpc_auth).unwrap();
 
+    //get mining info
     let mining_info = rpc.get_mining_info().unwrap();
     println!("{:?}", mining_info);
+
+    //get balance
+    let balance = rpc.get_balance(None, None).unwrap();
+    let sat_balance = balance.as_sat();
+    let btc_balance = balance.as_btc();
+    println!("sats: {:?}", sat_balance);
+    println!("btc: {:?}", btc_balance);
 }
