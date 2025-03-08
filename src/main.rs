0b647c164f474bb288e91f4f5e821e431a8f40b9
j-pinto <jfpinto1989@gmail.com>
2025-03-07 22:27:19 -0500

    make and send transaction

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use bitcoincore_rpc::{ bitcoin::{util::amount, Amount}, json, Auth, Client, RpcApi};
+use std::collections::HashMap;
+
+use bitcoincore_rpc::{ bitcoin::Amount, json, Auth, Client, RpcApi};
 fn main() {
     //connect to node
     let rpc_auth = Auth::UserPass("user".to_string(), "password".to_string());
@@ -22,7 +24,7 @@ fn main() {
         .unwrap();
     println!("my address: {:?}", my_address);
 
-    //select first UTXO
+    //list all UTXOs
     let unspent = rpc
         .list_unspent(
             None,
@@ -38,7 +40,43 @@ fn main() {
         )
         .unwrap();
     
+    //select first UTXO to use in the transaction
     let selected_tx = &unspent[0];
     
     println!("selected unspent transaction: {:#?}", selected_tx);
+
+    //make transaction inputs
+    let txid = selected_tx.txid;
+    let vout = selected_tx.vout;
+    let tx_input = json::CreateRawTransactionInput {
+        txid: txid,
+        vout: vout,
+        sequence: None,
+    };
+
+    //make transaction outputs
+    let amount = selected_tx.amount;
+    let fee = Amount::from_sat(1000);
+    let amount_to_spend = amount - fee;
+    let mut tx_output = HashMap::new();
+    tx_output.insert(my_address.to_string(), amount_to_spend);
+
+    //make unsigned transaction
+    let unsigned_tx = rpc
+        .create_raw_transaction(&[tx_input], &tx_output, None, None)
+        .unwrap();
+    
+    //sign transaction
+    let signed_tx = rpc
+        .sign_raw_transaction_with_wallet(&unsigned_tx, None, None)
+        .unwrap();
+    
+    println!("signed tx {:?}", signed_tx.transaction().unwrap());
+
+    //send transaction
+    let txid_sent = rpc
+        .send_raw_transaction(&signed_tx.transaction().unwrap())
+        .unwrap();
+
+    println!("{:?}", txid_sent);
 }
